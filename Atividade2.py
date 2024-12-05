c905cf5660d7e0b70ced7558abd0820a3e6cead9
SidneyOliveiraDev <165084944+SidneyOliveiraDev@users.noreply.github.com>
2024-12-05 09:47:28 -0300

    Delete Atividade2.py

--- Atividade2.py
+++
@@ -1,9 +0,0 @@
-print("Hello world!!")
-
-nome_usuario = 'Gocu' # Tipo -> Str = String
-idade_usuario = 48 # Tipo -> int = integer
-altura_usuario = 1.82 # Tipo -> float = ponto flutuante
-maior_idade = True # Tipo -> bool = Boolean
-
-print('Seja bem-vindo', nome_usuario, 'sua idade é', idade_usuario, 'anos, você tem:', altura_usuario,
-'metros. Você é maior de idade?', maior_idade)
